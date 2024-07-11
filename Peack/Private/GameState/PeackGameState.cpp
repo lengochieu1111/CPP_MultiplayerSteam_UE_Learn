9c72d08a3239bb6f07a4eec92047a59502af33aa
Le Ngoc Hieu <lengochieutlu11112003.com>
2024-07-11 16:57:29 +0700

    89 - Peack Game State

---
+++ Peack/Private/GameState/PeackGameState.cpp
@@ -0,0 +1,8 @@
+
+#include "GameState/PeackGameState.h"
+
+APeackGameState::APeackGameState()
+{
+	this->NetUpdateFrequency = 10.0f;
+	this->MinNetUpdateFrequency = 7.0f;
+}
---
+++ Peack/Public/GameState/PeackGameState.h
@@ -0,0 +1,21 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "PeackGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PEACK_API APeackGameState : public AGameState
+{
+	GENERATED_BODY()
+
+	// FUNCTION
+
+public:
+	APeackGameState();
+	
+};
